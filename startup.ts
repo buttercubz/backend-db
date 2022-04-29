54bb316caefaed777f8c316ea96a085c3a67123b
buttercubz <ericksosagarcias@gmail.com>
2022-04-29 12:21:48 -0400

    fix: add cors

--- startup.ts
+++ startup.ts
@@ -3,6 +3,22 @@ import Router from "./src/router/router.ts";
 
 const app = new Application();
 
+// for cors
+app.use(async (ctx, next) => {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+  ctx.response.headers.set(
+    "Access-Control-Allow-Methods",
+    "POST, PUT, GET, OPTIONS"
+  );
+
+  ctx.response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+  );
+
+  await next();
+});
+
 app.use(Router.routes());
 app.use(Router.allowedMethods());
 
